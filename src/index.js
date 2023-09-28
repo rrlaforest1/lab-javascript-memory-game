71963f31b51cbe1841dca7ab955e0c5dec3c5270
Rony Laforest <rrlaforest@gmail.com>
2023-09-28 17:37:21 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 const cards = [
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' },
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
 ];
 
+/**
+ * all my consts
+ */
 const memoryGame = new MemoryGame(cards);
+const pairsClickedCounter = document.querySelector("#pairs-clicked");
+const pairsGuessedCounter = document.querySelector("#pairs-guessed");
+const winModal = document.querySelector("#winModal");
+const playAgain = winModal.querySelector("button");
 
-window.addEventListener('load', (event) => {
-  let html = '';
+let cardHolder = [];
+
+window.addEventListener("load", (event) => {
+  prepareGame();
+});
+
+/**
+ * Created a function to used it with the "play again button"
+ */
+function prepareGame() {
+  let html = "";
+  memoryGame.shuffleCards();
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -39,13 +56,37 @@ window.addEventListener('load', (event) => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  document.querySelector("#memory-board").innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", (e) => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log(`Card clicked: ${card}`);
+
+      const currentCard = e.target.closest(".card");
+      currentCard.classList.add("turned");
+
+      /**
+       * limit the number of cards to 2
+       */
+      cardHolder.push(currentCard);
+
+      if (cardHolder.length === 2) {
+        const pairChecked = memoryGame.checkIfPair(
+          cardHolder[0].getAttribute("data-card-name"),
+          cardHolder[1].getAttribute("data-card-name")
+        );
+        memoryGame.updatedPairsClicked();
+        if (pairChecked) {
+          memoryGame.samePair(cardHolder[0], cardHolder[1]);
+        } else {
+          memoryGame.differentPair(cardHolder[0], cardHolder[1]);
+        }
+        cardHolder = [];
+      }
     });
   });
-});
+}
+
+playAgain.addEventListener("click", memoryGame.newGame);
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,96 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) return undefined;
+
+    /**
+     * make random to be between i and the array length without repeating it
+     */
+    let nums = structuredClone(this.cards),
+      ranNums = [],
+      i = nums.length,
+      j = 0;
+
+    while (i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      ranNums.push(nums[j]);
+      nums.splice(j, 1);
+    }
+
+    this.cards = ranNums;
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+
+    /**
+     * if both cards are the same, block them. else turn them back with a timeout for better UX
+     */
+    if (card1 === card2) {
+      this.pairsGuessed++;
+    }
+
+    return card1 === card2;
+  }
+
+  samePair(card1, card2) {
+    this.pairsGuessed++;
+    card1.classList.add("blocked");
+    card2.classList.add("blocked");
+    this.updatedPairsGuessed();
+
+    /**
+     * if finished win mesage
+     */
+    if (this.checkIfFinished()) {
+      this.winMessage();
+    }
+  }
+
+  differentPair(card1, card2) {
+    setTimeout(() => {
+      card1.classList.remove("turned");
+      card2.classList.remove("turned");
+    }, 1000);
+  }
+
+  // Update the counters on the page
+  updatedPairsClicked() {
+    pairsClickedCounter.textContent = this.pairsClicked;
+  }
+  updatedPairsGuessed() {
+    pairsGuessedCounter.textContent = this.pairsGuessed;
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  winMessage() {
+    winModal.showModal();
+  }
+
+  // Reset all values and recreate board (with suffled cards)
+  newGame() {
+    prepareGame();
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    updatedPairsClicked();
+    updatedPairsGuessed();
+    winModal.hide();
   }
 }
